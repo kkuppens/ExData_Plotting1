45c1e5a9a539103fa6850a58f6518b3bcb795744
kkuppens <kkuppens@hotmail.com>
2014-06-02 19:16:09 +0200

    Updated plot3.R

--- plot3.R
+++ plot3.R
@@ -9,7 +9,7 @@ data <- subset(data, as.Date(datetime) >= as.Date("2007-02-01") &
                  as.Date(datetime) <= as.Date("2007-02-02"))
 
 #Step 3. Create plot of Global Active Power over date and time and display to sceen device.
-png(file = "plot3a.png",height=480, width=480)
+png(file = "plot3.png",height=480, width=480)
 
 plot(data$datetime, data$Sub_metering_1, type="l",  xlab = "", ylab="Energy sub metering", col = "black")
 points(data$datetime, data$Sub_metering_2, type="l", col="red")
