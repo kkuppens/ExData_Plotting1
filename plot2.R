e348cbf92dfbfbdfa5a90566b94a0633ade9cb03
kkuppens <kkuppens@hotmail.com>
2014-06-02 19:09:48 +0200

    Updated plot3.R

--- plot2.R
+++ plot2.R
@@ -1,14 +1,18 @@
 #Step 1. Load all data from file into R
-data <- read.csv('./data/household_power_consumption.txt', sep=";", na.strings="?")
-#Step 2. Filter data set by the requested dates using a logical vector
-data <- data[(data$Date == "1/2/2007") | (data$Date == "2/2/2007"), ]
-#Step 3. Create date and time stamps  
-x <- paste(data$Date, data$Time,sep = " ")
-x <- strptime(x, "%d/%m/%Y %H:%M:%S")
-#Step 4. Create plot of Global Active Power over date and time and display to sceen device.
+data <- read.csv('./data/household_power_consumption.txt', sep=";", na.strings="?",header=T)
+
+#Step 2. Filter data set by the requested dates using subset and logical condition
+# Create datetime variable and add to data frame.
+data$datetime <- strptime(paste(data$Date, data$Time, sep = " "), "%d/%m/%Y %H:%M:%S")
+# Select data for first and second of February 2007
+data <- subset(data, as.Date(datetime) >= as.Date("2007-02-01") &
+                 as.Date(datetime) <= as.Date("2007-02-02"))
+#Create png graphic device with resolution 480x480 
+png(file = "plot2.png", height=480, width=480)
+#Step 3. Create histogram and display to screen device (png file).
 #Add description for y-axis, remove description for x-axis, use type lines.
-plot(x, data$Global_active_power, type="l", xlab ="", ylab="Global Active Power (kilowatts)")
-#Step 4. Make copy of screen device output to file of type png, named plot2.png
-#File will be stored in users default primary work directory, see getwd().
-dev.copy(png, file = "plot2.png")
-dev.off()
+plot(data$datetime, data$Global_active_power, type="l", xlab ="", ylab="Global Active Power (kilowatts)")
+dev.off() #close device
+
+
+
