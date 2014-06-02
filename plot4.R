a86961d16af5c282561471caa34958e780c02dc6
kkuppens <kkuppens@hotmail.com>
2014-06-02 20:15:18 +0200

    Updated plot4.R

--- plot4.R
+++ plot4.R
@@ -20,7 +20,7 @@ plot(data$datetime, data$Sub_metering_1, type="l",  xlab = "", ylab="Energy sub 
 points(data$datetime, data$Sub_metering_2, type="l", col="red")
 points(data$datetime, data$Sub_metering_3, type="l", col="blue")
 legend("topright", lwd=c(1, 1, 1), legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), 
-       col=c("black","red","blue"))
+       col=c("black","red","blue"), bty = "n")
 #plot 4, right bottom
 plot(data$datetime, data$Global_reactive_power, type="l", col="black", xlab ="datetime", 
      ylab="Global_reactive_power")
